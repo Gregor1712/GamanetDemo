0c98170f6ce60e9eeb33beb0bb347d9a72116ac3
Gregor Pavlik <gregor.pavlik@gmail.com>
2026-05-17 12:05:07 +0200

    dfgdfg

--- GamanetDemo/Model/PersonRepository.cs
+++ GamanetDemo/Model/PersonRepository.cs
@@ -15,14 +15,13 @@ internal class PersonRepository
         Persons = new ObservableCollection<PersonEntity>();
     }
 
-    public async Task<bool> LoadDataAsync()
+    public async Task LoadDataAsync()
     {
         var persons = await new CsvDataSource(_appContext).LoadPersonsAsync();
-
         Persons.Clear();
         foreach (var item in persons)
-            Persons.Add(item);
-
-        return true;
+        {
+            Persons.Add(item);  
+        }
     }
 }
